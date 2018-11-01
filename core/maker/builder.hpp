f01225c9d116d54b0612024af198b659184bea03
coder3101 <ashar786khan@gmail.com>
2018-11-01 23:44:26 +0530

    deleted copy, move, assisgnment of testcase builder.

--- core/maker/builder.hpp
+++ core/maker/builder.hpp
@@ -23,6 +23,9 @@
 #include <iostream>
 #include <random>
 #include <string>
+
+#define NEW_LINE ('\n')
+
 namespace testcaser {
 namespace maker {
 class TestCaseBuilder {
@@ -32,7 +35,9 @@ class TestCaseBuilder {
 
  public:
   TestCaseBuilder() = delete;
-  TestCaseBuilder(std::string& abs_name) : fname(abs_name) {
+  TestCaseBuilder(TestCaseBuilder&) = delete;
+  TestCaseBuilder(TestCaseBuilder&&) = delete;
+  explicit TestCaseBuilder(std::string abs_name) : fname(abs_name) {
     file = std::ofstream(abs_name, std::ios::out);
     if (!file.is_open() || !file.good()) {
       throw testcaser::exceptions::maker::TestFileIOError(
@@ -44,80 +49,194 @@ class TestCaseBuilder {
 
   template <class gen = std::mt19937,
             class dis = std::uniform_int_distribution<long long>>
-  TestCaseBuilder& add_new_line(types::RandomAlphabet<gen, dis>& ra,
-                                char end_with = '\n') {
+  char add_new(types::RandomAlphabet<gen, dis>& ra, bool ender,
+               bool use_only_lower = false,
+               bool use_only_upper = false, char end_with = '\n') {
     if (this->finalized) {
       throw testcaser::exceptions::maker::FinalizationError(
-          "Cannot add new line once file has been finalized");
+          "Cannot add new alphabet once file has been finalized");
     }
-    file << ra.get() << end_with;
-    return *this;
+    char u = ra.get();
+    char v = ra.get_lower();
+    char s = ra.get_upper();
+    if (ender) {
+      if (use_only_lower == use_only_upper) {
+        file << u << end_with;
+        return u;
+      } if (use_only_lower) {
+        file << v << end_with;
+        return v;
+      } if (use_only_upper) {
+        file << s << end_with;
+        return s;
+      }
+    } else {
+      if (use_only_upper == use_only_lower) {
+        file << u;
+        return u;
+      } if (use_only_lower) {
+        file << v;
+        return v;
+      } if (use_only_upper) {
+        file << s;
+        return s;
+      }
+    }
   };
   template <class gen = std::mt19937,
             class dis = std::uniform_int_distribution<long long>>
-  TestCaseBuilder& add_new_line(types::RandomInteger<gen, dis>& ri,
-                                char end_with = '\n') {
+  long long add_new(types::RandomInteger<gen, dis>& ri, bool ender,
+                    char end_with = '\n') {
     if (this->finalized) {
       throw testcaser::exceptions::maker::FinalizationError(
           "Cannot add new line once file has been finalized");
     }
-    file << ri.get() << end_with;
-    return *this;
+    long long res = ri.get();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
   };
   template <class gen = std::mt19937,
             class dis = std::uniform_int_distribution<unsigned long long>>
-  TestCaseBuilder& add_new_line(types::RandomUnsignedInteger<gen, dis>& rui,
-                                char end_with = '\n') {
+  unsigned long long add_new(types::RandomBinary<gen, dis>& ri,
+                             bool ender, char end_with = '\n') {
+    if (this->finalized) {
+      throw testcaser::exceptions::maker::FinalizationError(
+          "Cannot add new binary line once file has been finalized");
+    }
+    unsigned long long res = ri.get_as_int();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
+  };
+  template <class gen = std::mt19937,
+            class dis = std::uniform_int_distribution<unsigned long long>>
+  unsigned long long add_new(types::RandomTernary<gen, dis>& ri,
+                             bool ender, char end_with = '\n') {
+    if (this->finalized) {
+      throw testcaser::exceptions::maker::FinalizationError(
+          "Cannot add new ternary line once file has been finalized");
+    }
+    unsigned long long res = ri.get_as_int();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
+  };
+  template <class gen = std::mt19937,
+            class dis = std::uniform_int_distribution<unsigned long long>>
+  unsigned long long add_new(types::RandomQuaternary<gen, dis>& ri,
+                             bool ender, char end_with = '\n') {
+    if (this->finalized) {
+      throw testcaser::exceptions::maker::FinalizationError(
+          "Cannot add new binary quaternary once file has been finalized");
+    }
+    unsigned long long res = ri.get_as_int();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
+  };
+  template <class gen = std::mt19937,
+            class dis = std::uniform_int_distribution<unsigned long long>>
+  unsigned long long add_new(types::RandomQuinary<gen, dis>& ri,
+                             bool ender, char end_with = '\n') {
+    if (this->finalized) {
+      throw testcaser::exceptions::maker::FinalizationError(
+          "Cannot add new quinary line once file has been finalized");
+    }
+    unsigned long long res = ri.get_as_int();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
+  };
+  template <class gen = std::mt19937,
+            class dis = std::uniform_int_distribution<unsigned long long>>
+  unsigned long long add_new(types::RandomSenary<gen, dis>& ri,
+                             bool ender, char end_with = '\n') {
+    if (this->finalized) {
+      throw testcaser::exceptions::maker::FinalizationError(
+          "Cannot add new senary line once file has been finalized");
+    }
+    unsigned long long res = ri.get_as_int();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
+  };
+  template <class gen = std::mt19937,
+            class dis = std::uniform_int_distribution<unsigned long long>>
+  unsigned long long add_new(types::RandomUnsignedInteger<gen, dis>& rui,
+                             bool ender, char end_with = '\n') {
     if (this->finalized) {
       throw testcaser::exceptions::maker::FinalizationError(
           "Cannot add new line once file has been finalized");
     }
-    file << rui.get() << end_with;
-    return *this;
+    unsigned long long res = rui.get();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
   };
   template <class T, class gen = std::mt19937,
             class dis = std::uniform_int_distribution<unsigned long long>>
-  TestCaseBuilder& add_new_line(types::RandomFrom<T, gen, dis>& rft,
-                                char end_with = '\n') {
+  T add_new(types::RandomFrom<T, gen, dis>& rft, bool ender,
+            char end_with = '\n') {
     if (this->finalized) {
       throw testcaser::exceptions::maker::FinalizationError(
           "Cannot add new line once file has been finalized");
     }
-    file << rft.get() << end_with;
-    return *this;
+    auto res = rft.get();
+    if (ender)
+      file << res << end_with;
+    else
+      file << res;
+    return res;
   };
 
-  TestCaseBuilder& add_single_space() {
+  void add_single_space() {
     if (this->finalized) {
       throw testcaser::exceptions::maker::FinalizationError(
           "Cannot add space once file has been finalized");
     }
     file << ' ';
-    return *this;
   };
-  TestCaseBuilder& add_special_character(char chr, char end_with = '\n') {
+  void add_special_character(char chr, bool ender,
+                             char end_with = '\n') {
     if (this->finalized) {
       throw testcaser::exceptions::maker::FinalizationError(
           "Cannot add special character once file has been finalized");
     }
-    file << chr << end_with;
-    return *this;
+    if (ender)
+      file << chr << end_with;
+    else
+      file << chr;
   };
-  TestCaseBuilder& add_new_line() {
+  void add_new_line() {
     if (this->finalized) {
       throw testcaser::exceptions::maker::FinalizationError(
           "Cannot add new line once file has been finalized");
     }
     file << std::endl;
-    return *this;
   }
-  TestCaseBuilder& finalize() {
+  void finalize() {
     this->finalized = true;
     file.close();
   }
   virtual ~TestCaseBuilder() {
     if (this->file.is_open()) this->file.close();
     if (!this->finalized) {
+      std::cerr << "Warning : Removing the unfinalized file " + fname + "\n";
       if (std::remove(this->fname.c_str()) != 0) {
         std::cerr << "Failed to remove Unfinalized file " << fname;
       };
