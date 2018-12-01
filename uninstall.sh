ae611c7cbb2c59c85369a505dd3dfdacf452a412
coder3101 <ashar786khan@gmail.com>
2018-12-01 17:31:06 +0530

    generated docs

--- uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash   
-set -e
-echo -e "\n***************STARTING THE UNINSTALLATION*****************"
-echo -e "\nScript Running Director is : "$PWD
-echo -e "\n1. Removing the symlink of testcaser from /usr/include/"
-rm /usr/include/testcaser
-echo -e "2. Uninstall Success\n"
-echo "*************************************************************"
