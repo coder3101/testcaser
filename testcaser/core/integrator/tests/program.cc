f2fd99fe3eefb0b758e8252c4737e330c84a135e
coder3101 <ashar786khan@gmail.com>
2018-12-15 17:57:45 +0530

    a dummy program to execute on virtual judge

---
+++ testcaser/core/integrator/tests/program.cc
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2018 Ashar <ashar786khan@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+long int mem[100];
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+  }
+  while(1);
+  return 0;
+}
