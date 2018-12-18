aeda3fb3c59a9c97c632fda930da699a04d1f72b
coder3101 <ashar786khan@gmail.com>
2018-12-18 21:26:53 +0530

    adding argument parser for cmd argument parsing task

---
+++ testcaser/source/argument_parser.hpp
@@ -0,0 +1,77 @@
+/**
+ * Copyright 2018 Ashar <ashar786khan@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ARGUMENT_PARSER_HPP
+#define ARGUMENT_PARSER_HPP
+
+#include <functional>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace testcaser {
+namespace internals {
+
+class ArgumentParser {
+  int count;
+  std::vector<std::string> args;
+
+ public:
+  ArgumentParser(int argc, char *argv[]) {
+    count = argc - 1;
+    for (int t = 1; t < argc; t++)
+      args.push_back(std::string(static_cast<const char *>(argv[t])));
+
+    if (!args.size() % 2)
+      throw std::length_error(
+          "Argument Count mismatch. Did you forgot to specifiy a value for a "
+          "flag.");
+  }
+
+  std::unordered_map<std::string, std::string> parse() {
+    std::unordered_map<std::string, std::string> map;
+    for (int t = 0; t < args.size() - 1; t += 2)
+      map[args[t].substr(2, std::string::npos)] = args[t + 1];
+    return map;
+  }
+
+  static bool handle_version(int argv, char *argc[],
+                             std::function<void()> callback) {
+    if (argv > 2) return false;
+    if (std::string(argc[1]) != "--version") return false;
+    callback();
+    return true;
+  }
+
+  static bool handle_help(int argv, char *argc[],
+                          std::function<void()> callback) {
+    if (argv == 1) {
+      std::cerr << "You must specify --program and --input for judge to run\n";
+      return true;
+    }
+    if (argv > 2)
+      return false;
+    else {
+      callback();
+      return true;
+    }
+  }
+};
+}  // namespace internals
+}  // namespace testcaser
+#endif
