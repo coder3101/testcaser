198a5c2d41773206e6d762a78129aaeedc60a2a0
coder3101 <ashar786khan@gmail.com>
2018-12-20 14:46:12 +0530

    docs generation for 0.3

--- docs/html/search/all_8.js
+++ docs/html/search/all_8.js
@@ -3,5 +3,7 @@ var searchData=
   ['intervals',['Intervals',['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html',1,'testcaser::maker::limits::Intervals&lt; T &gt;'],['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html#a69fe886908002aa20f41ed886fc213ef',1,'testcaser::maker::limits::Intervals::Intervals(T a, T b)'],['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html#ad899dc031cbffd5247b9b39e23577a52',1,'testcaser::maker::limits::Intervals::Intervals(std::pair&lt; T, T &gt; pp)']]],
   ['intervals_3c_20int_20_3e',['Intervals&lt; int &gt;',['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html',1,'testcaser::maker::limits']]],
   ['intervals_3c_20long_20long_20_3e',['Intervals&lt; long long &gt;',['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html',1,'testcaser::maker::limits']]],
-  ['intervals_3c_20unsigned_20long_20long_20_3e',['Intervals&lt; unsigned long long &gt;',['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html',1,'testcaser::maker::limits']]]
+  ['intervals_3c_20unsigned_20long_20long_20_3e',['Intervals&lt; unsigned long long &gt;',['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html',1,'testcaser::maker::limits']]],
+  ['is_5freadable_5fbinary',['is_readable_binary',['../structtestcaser_1_1internal_1_1executor__engine.html#ac4add5efeeecea04a65f06c5f32f62fe',1,'testcaser::internal::executor_engine']]],
+  ['is_5freadable_5ffile',['is_readable_file',['../structtestcaser_1_1internal_1_1executor__engine.html#a3649324b1da2b5fc41627ca7a73135f4',1,'testcaser::internal::executor_engine']]]
 ];
--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['intervals',['Intervals',['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html#a69fe886908002aa20f41ed886fc213ef',1,'testcaser::maker::limits::Intervals::Intervals(T a, T b)'],['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html#ad899dc031cbffd5247b9b39e23577a52',1,'testcaser::maker::limits::Intervals::Intervals(std::pair&lt; T, T &gt; pp)']]]
+  ['intervals',['Intervals',['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html#a69fe886908002aa20f41ed886fc213ef',1,'testcaser::maker::limits::Intervals::Intervals(T a, T b)'],['../structtestcaser_1_1maker_1_1limits_1_1Intervals.html#ad899dc031cbffd5247b9b39e23577a52',1,'testcaser::maker::limits::Intervals::Intervals(std::pair&lt; T, T &gt; pp)']]],
+  ['is_5freadable_5fbinary',['is_readable_binary',['../structtestcaser_1_1internal_1_1executor__engine.html#ac4add5efeeecea04a65f06c5f32f62fe',1,'testcaser::internal::executor_engine']]],
+  ['is_5freadable_5ffile',['is_readable_file',['../structtestcaser_1_1internal_1_1executor__engine.html#a3649324b1da2b5fc41627ca7a73135f4',1,'testcaser::internal::executor_engine']]]
 ];
