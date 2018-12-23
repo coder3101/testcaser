8a88e89b5fd9f4075973c4bd594067317deac84b
coder3101 <ashar786khan@gmail.com>
2018-12-23 15:33:29 +0530

    docs updated for 0.3.5

--- docs/html/search/all_14.js
+++ docs/html/search/all_14.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['_7ebaseexception',['~BaseException',['../classtestcaser_1_1exceptions_1_1BaseException.html#adf792df62deef0ec4f4c1e34c59179fa',1,'testcaser::exceptions::BaseException']]],
   ['_7etestcasebuilder',['~TestCaseBuilder',['../classtestcaser_1_1maker_1_1TestCaseBuilder.html#ad463fd2e4c6175c15c310c4af66ec344',1,'testcaser::maker::TestCaseBuilder']]]
 ];
