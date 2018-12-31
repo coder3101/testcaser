7db31314ffc6b93ef60d3e4b62fda9397e7aad29
coder3101 <ashar786khan@gmail.com>
2018-12-31 12:47:45 +0530

    update current uninstall session

--- uninstall.bat
+++ uninstall.bat
@@ -2,6 +2,6 @@
 echo **************************************
 echo Removing from PATH Variables
 setx /M PATH "%PATH:%cd%\bin;=%"
+set PATH=%PATH%
 echo Removed from PATH Variables. 
-echo Close this terminal to see effect.
 echo ***************************************
