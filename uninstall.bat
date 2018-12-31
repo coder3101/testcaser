cf90bae6d6560e3f6264098363cff91c57da71f0
coder3101 <ashar786khan@gmail.com>
2018-12-31 12:59:51 +0530

    using delayed expansion

--- uninstall.bat
+++ uninstall.bat
@@ -1,6 +1,7 @@
 @echo off
 echo **************************************
 echo Removing from PATH Variables
+setlocal enableextensions enabledelayedexpansion
 setx /M PATH "%PATH:"%cd%\bin";=%"
 set PATH=%PATH%
 echo Removed from PATH Variables. 
