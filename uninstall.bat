724b6b5371bdce1d70cb7d1b036608b67fc054ff
coder3101 <ashar786khan@gmail.com>
2018-12-31 13:28:59 +0530

    remove uinstall failures

--- uninstall.bat
+++ uninstall.bat
@@ -1,8 +1,9 @@
 @echo off
 echo **************************************
 echo Removing from PATH Variables
-setlocal enableextensions enabledelayedexpansion
-setx /M PATH "%PATH:"%cd%\bin";=%"
-set PATH=%PATH%
-echo Removed from PATH Variables. 
+echo This script does not works as of now. Please remove manually by modifying from Environment Variable.
+REM setlocal enableextensions enabledelayedexpansion
+REM setx /M PATH "%PATH:"%cd%\bin";=%"
+REM set PATH=%PATH%
+REM echo Removed from PATH Variables. 
 echo ***************************************
