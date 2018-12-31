c17374954a5b58132226f585f087f0b1811449c5
coder3101 <ashar786khan@gmail.com>
2018-12-31 12:26:31 +0530

    in quotes

--- install.bat
+++ install.bat
@@ -3,6 +3,6 @@ echo *******************************************
 echo Begining the VirtualJudge Installtion.
 set PATH=%PATH%;%cd%\bin\
 echo %PATH%
-setx /M PATH %PATH%
+setx /M PATH "%PATH%"
 echo Installation Completed.
 echo *******************************************
