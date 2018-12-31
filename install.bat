bf7662b7450202126011cc8f8f20819fbc8c781d
coder3101 <ashar786khan@gmail.com>
2018-12-31 12:22:32 +0530

    adding to path with cwd

--- install.bat
+++ install.bat
@@ -1,7 +1,8 @@
 @echo on
 echo *******************************************
 echo Begining the VirtualJudge Installtion.
-set PATH=%PATH%;.\bin\
+set PATH=%PATH%;%cd%\bin\
+echo %PATH%
 setx /M PATH %PATH%
 echo Installation Completed.
 echo *******************************************
--- uninstall.bat
+++ uninstall.bat
@@ -1,6 +1,6 @@
 @echo on
 echo **************************************
 echo Removing from PATH Variables
-setx /M PATH "%PATH:.\bin;=%"
+setx /M PATH "%PATH:%cd%\bin;=%"
 echo Removed from PATH Variables. Close this terminal to see effect.
 echo ***************************************
