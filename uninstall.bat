c5481df75eaf4522c44154f5106298ae52c3bfe5
coder3101 <ashar786khan@gmail.com>
2019-01-03 12:17:55 +0530

    Todo for Failing Tasks

--- uninstall.bat
+++ uninstall.bat
@@ -6,4 +6,8 @@ REM setlocal enableextensions enabledelayedexpansion
 REM setx /M PATH "%PATH:"%cd%\bin";=%"
 REM set PATH=%PATH%
 REM echo Removed from PATH Variables. 
+REM /* @todo Fails to Remove the Environment Variable from System
+REM  * @body The Install Script and Uninstall Scripts are not working
+REM  * in the Windows. This issue is related to #15 
+REM  */
 echo ***************************************
