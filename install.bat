3df68a73e5ee6217f68ac205f4d4156420bbd6f9
coder3101 <ashar786khan@gmail.com>
2019-01-18 01:45:25 +0530

    stop windows install as it could be potential issue

--- install.bat
+++ install.bat
@@ -1,16 +1,16 @@
 @echo off
 echo *******************************************
-echo 1. Begining the VirtualJudge Installtion.
-echo 2. Taking BACKUP of current PATH.
-echo %PATH% > old_path.txt
-echo 3. Backup Done, You can use this to restore your path incase of failures.
-echo 4. Backup was saved as old_path.txt in current directory.
-echo 5. Setting new PATH Variable for current Session.
-set PATH=%PATH%;%cd%\bin\
-echo 6. Setting new PATH Variable for other Session.
-setx /M PATH "%PATH%"
-echo NOTE : Installation Completed. Please Note if you see a warning 
-echo PATH WAS TRUNCATED TO 1024, It means your environment variables are 
-echo Corrupted. Please Restore your old PATH from backup and manually
-echo add %cd%\bin to your PATH Variables. 
+REM echo 1. Begining the VirtualJudge Installtion.
+REM echo 2. Taking BACKUP of current PATH.
+REM echo %PATH% > old_path.txt
+REM echo 3. Backup Done, You can use this to restore your path incase of failures.
+REM echo 4. Backup was saved as old_path.txt in current directory.
+REM echo 5. Setting new PATH Variable for current Session.
+REM set PATH=%PATH%;%cd%\bin\
+REM echo 6. Setting new PATH Variable for other Session.
+REM setx /M PATH "%PATH%"
+REM echo NOTE : Installation Completed. Please Note if you see a warning 
+REM echo PATH WAS TRUNCATED TO 1024, It means your environment variables are 
+REM echo Corrupted. Please Restore your old PATH from backup and manually
+REM echo add %cd%\bin to your PATH Variables. 
 echo *******************************************
