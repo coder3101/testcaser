7caaff00c20aafc3b786cf96c1887b060aa0101b
coder3101 <ashar786khan@gmail.com>
2019-02-07 16:01:57 +0530

    Documenation update

--- docs/html/search/functions_b.js
+++ docs/html/search/functions_b.js
@@ -2,10 +2,10 @@ var searchData=
 [
   ['set_5fauto_5fexit',['set_auto_exit',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#a9160dd070c63084495fe6d29cab58cb4',1,'testcaser::integrator::VirtualJudge']]],
   ['set_5fauto_5fexit_5ftime_5flimit',['set_auto_exit_time_limit',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#ac739270769d85fd09ae35aa53726f8f4',1,'testcaser::integrator::VirtualJudge']]],
-  ['set_5fbinary',['set_binary',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#ae9fc3d7bf1bf75fc1ec695fe831d16b6',1,'testcaser::integrator::VirtualJudge']]],
-  ['set_5finput_5ffile',['set_input_file',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#ad982d30de1a2f9033cb042313d748292',1,'testcaser::integrator::VirtualJudge']]],
+  ['set_5fbinary',['set_binary',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#aae183fb21dfffe38efcfb904d482b299',1,'testcaser::integrator::VirtualJudge']]],
+  ['set_5finput_5ffile',['set_input_file',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#a2ac1d116076a4ee8fde3ddd1f0849309',1,'testcaser::integrator::VirtualJudge']]],
   ['set_5fmemory_5flimit',['set_memory_limit',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#a8ac8e323d9f69fb4e1f6ee2e9aa1ae9f',1,'testcaser::integrator::VirtualJudge']]],
-  ['set_5foutput_5ffile',['set_output_file',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#a9f054aac69019e5f6bde646ccc72effb',1,'testcaser::integrator::VirtualJudge']]],
+  ['set_5foutput_5ffile',['set_output_file',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#ac806c80611eb6e6049f3376790d4b42a',1,'testcaser::integrator::VirtualJudge']]],
   ['set_5fresolution',['set_resolution',['../classtestcaser_1_1exceptions_1_1BaseException.html#a92c371d40b0f3dbaa7de968ea67e5211',1,'testcaser::exceptions::BaseException']]],
   ['set_5ftime_5flimit',['set_time_limit',['../classtestcaser_1_1integrator_1_1VirtualJudge.html#a121272932bb115881ad90be79ad7d0a7',1,'testcaser::integrator::VirtualJudge']]]
 ];
